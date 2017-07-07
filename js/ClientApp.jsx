59ec1c27ca45b31ff735cd28209e0fb7b69a46c9
vanessa <vanessa@snapzio.com>
2017-07-07 13:34:17 +0800

    Initial commit

---
+++ js/ClientApp.jsx
@@ -0,0 +1,23 @@
+import React from 'react'; // default export for 'react'
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
+
+const FourOhFour = () => (
+  <h1>404</h1>
+);
+
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/search" component={Search} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+  );
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ js/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ShowCard from './ShowCard';
+import preload from '../data.json';
+
+const Search = () => (
+  <div className="search">
+    {preload.shows.map(show => (
+      <ShowCard {...show} />
+    ))}
+  </div>
+);
+
+export default Search;
